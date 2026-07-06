935ce83b8b85baf8ad296c85b26d3658f274ae23
Mauricio Offermann <moffermann@gmail.com>
2026-07-06 08:17:38 -0400

    Add behavioral ABM package init

---
+++ experiments/behavioral-adoption-abm/src/behavioral_adoption_abm/__init__.py
@@ -0,0 +1,5 @@
+"""Core v0 behavioral adoption ABM package."""
+
+from .core import BehavioralAdoptionModel, load_config, run_scenario
+
+__all__ = ["BehavioralAdoptionModel", "load_config", "run_scenario"]
